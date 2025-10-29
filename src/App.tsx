fcff24018f962d35fbb8624cc91f0db993fed195
Anton Kasaryn <kasarin.anton@gmail.com>
2025-10-29 14:49:27 +0100

    Add basic view with city-weather cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { CityListItem } from './components/CityListItem'
-import {DATA} from './mockData'
+import { DATA } from './mockData'
 
 function App() {
-
-  return ( 
-  <div className='w-full flex items-center flex-col gap-2 p-8'>
-    <input className="border border-neutral-400 p-2 rounded-md w-full" />
+  return (
+    <div className='w-full flex items-center flex-col gap-2 p-8'>
     {DATA.map((city) => <CityListItem {...city} />)}
   </div>
   )
--- src/components/CityListItem.tsx
+++ src/components/CityListItem.tsx
@@ -7,16 +7,18 @@ interface CityListItemProps {
 }
 
 export const CityListItem = ({ icon, cityName, countryCode, temperature, weatherDescription } : CityListItemProps) => {
-    return <div className="w-100 h-fit rounded-md border border-neutral-400 flex justify-between p-2 items-center shadow-md">
-        <div className="flex item-center">
-            <span>(icon)</span>
-            <div className="flex flex-col">
-                <span>(cityName), (countryCode)</span>
-                <span>(weatherDescription)</span>
+    return <div className="w-200 h-fit rounded-lg flex justify-between p-2 items-center shadow-md bg-white dark:bg-[#111618] shadow-sm">
+        <div className="flex items-center gap-4">
+            <div className="flex items-center justify-center rounded-lg bg-background-light dark:bg-[#283339] size-12">
+                <span>{icon}</span>
+            </div>
+            <div className="flex flex-col justify-center">
+                <span className="text-gray-800 dark:text-white">{cityName}</span>
+                <span className="text-gray-600 dark:text-[#9db0b9]">{temperature}°C</span>
             </div>
         </div>
         <div>
-            <span className="text-xl font bold">(temperature)</span>
+            <button className="flex items-center justify-center  px-4 bg-[#283339] text-white text-sm hover:bg-gray-700">View details</button>
         </div>
     </div>
 }
