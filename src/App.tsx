77e3f2cf794e2e1d327d95473f6a825834a00346
Anton Kasaryn <kasarin.anton@gmail.com>
2025-10-31 10:52:28 +0100

    Add full UI for home screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,40 @@ import { DATA } from './mockData'
 
 function App() {
   return (
-    <div className='w-full flex items-center flex-col gap-2 p-8'>
-    {DATA.map((city) => <CityListItem {...city} />)}
-  </div>
+    <div className="flex flex-col w-full items-center justify-center">
+      <header className="w-300 flex items-center justify-between border-b border-solid border-b-[#283339] px-4 py-3 bg-[#111618] rounded-lg">
+        <div className="flex items-center gap-4 text-white">
+                <div className="size-8 text-primary">
+                  <span className="material-symbols-outlined">thermostat</span>
+                </div>
+                <h2 className="text-white text-xl font-bold">WeatherNow</h2>
+              </div>
+
+              <div className="flex justify-end items-center gap-4">
+                <div className="flex w-full flex-1 items-stretch rounded-lg h-full">
+                  <div className="text-[#9db0b9] flex bg-[#283339] items-center justify-center pl-4 rounded-l-lg border-r-0">
+                    <span className="material-symbols-outlined">search</span>
+                  </div>
+                  <input className="form-input flex rounded-lg text-white focus:outline-0 focus:ring-0 border-none bg-background-light bg-[#283339] focus:border-none h-full placeholder:text-[#9db0b9] px-4 rounded-l-none  pl-2" placeholder="Search for a city..." />
+                </div>
+                <button className="flex items-center justify-center rounded-lg px-4 bg-primary text-white text-sm font-bold hover:bg-primary/90">
+                  Add City
+                </button>
+              </div>
+      </header>
+
+      <div className='flex items-center flex-col gap-2 p-8'>
+        {DATA.map((city) => <CityListItem {...city} />)}
+      </div>
+
+      <footer className="w-full flex flex-col gap-6 px-5 py-10 text-center @container mt-auto">
+              <div className="flex items-center justify-center gap-6">
+                <a className="text-[#9db0b9] leading-normal min-w-40 hover:text-primary dark:hover:text-primary" href="#">About</a>
+                <a className="text-[#9db0b9] leading-normal min-w-40 hover:text-primary dark:hover:text-primary" href="#">Contact</a>
+              </div>
+              <p className="text-gray-500 dark:text-[#9db0b9] text-base font-normal leading-normal">© 2024 WeatherNow</p>
+        </footer>
+    </div>
   )
 }
 
--- src/components/CityListItem.tsx
+++ src/components/CityListItem.tsx
@@ -7,14 +7,14 @@ interface CityListItemProps {
 }
 
 export const CityListItem = ({ icon, cityName, countryCode, temperature, weatherDescription } : CityListItemProps) => {
-    return <div className="w-200 h-fit rounded-lg flex justify-between p-2 items-center shadow-md bg-white dark:bg-[#111618] shadow-sm">
+    return <div className="w-200 h-fit rounded-lg flex justify-between p-2 items-center shadow-md bg-[#111618] shadow-sm">
         <div className="flex items-center gap-4">
             <div className="flex items-center justify-center rounded-lg bg-background-light dark:bg-[#283339] size-12">
                 <span>{icon}</span>
             </div>
             <div className="flex flex-col justify-center">
-                <span className="text-gray-800 dark:text-white">{cityName}</span>
-                <span className="text-gray-600 dark:text-[#9db0b9]">{temperature}°C</span>
+                <span className="text-white">{cityName}</span>
+                <span className="text-[#9db0b9]">{temperature}°C</span>
             </div>
         </div>
         <div>
