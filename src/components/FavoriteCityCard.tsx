523054cedf223c612b6e820f718a842dec57a173
Anton Kasaryn <kasarin.anton@gmail.com>
2025-11-04 13:56:47 +0100

    Add link to weather details from favourite weather

--- src/components/FavoriteCityCard.tsx
+++ src/components/FavoriteCityCard.tsx
@@ -1,5 +1,6 @@
 import type { CityWeather } from '../mockData';
 import { useFavorites } from '../contexts/FavoritesContext';
+import { Link } from 'react-router-dom';
 
 interface FavoriteCityCardProps {
   city: CityWeather;
@@ -9,29 +10,31 @@ export const FavoriteCityCard = ({ city }: FavoriteCityCardProps) => {
   const { toggleFavorite } = useFavorites();
 
   return (
-    <div className="flex flex-col gap-4 card-light dark:bg-card-dark p-6 rounded-lg hover:shadow-lg transition-shadow duration-300">
-      <div className="flex items-center gap-4">
-        <div className="text-accent flex items-center justify-center rounded-full bg-accent/20 shrink-0 size-12">
-          <span className="text-accent material-symbols-outlined text-3xl">{city.iconName}</span>
+    <Link to={`/details/${city.id}`}>
+        <div className="flex flex-col gap-4 card-light dark:bg-card-dark p-6 rounded-lg hover:shadow-lg transition-shadow duration-300">
+        <div className="flex items-center gap-4">
+            <div className="text-accent flex items-center justify-center rounded-full bg-accent/20 shrink-0 size-12">
+            <span className="text-accent material-symbols-outlined text-3xl">{city.iconName}</span>
+            </div>
+
+            <div className="flex flex-col justify-center">
+            <p className="text-xl text-white font-bold leading-normal">{city.cityName}</p>
+            <p className="text-sm font-normal leading-normal text-gray-500 dark:text-gray-400">
+                {city.temperature}°C, {city.weatherDescription}
+            </p>
+            </div>
         </div>
 
-        <div className="flex flex-col justify-center">
-          <p className="text-xl font-bold leading-normal">{city.cityName}</p>
-          <p className="text-sm font-normal leading-normal text-gray-500 dark:text-gray-400">
-            {city.temperature}°C, {city.weatherDescription}
-          </p>
+        <div className="flex justify-end">
+            <button 
+            onClick={() => toggleFavorite(city.id)}
+            className="flex items-center gap-2 text-sm font-medium text-danger hover:text-danger-strong dark:hover:text-danger"
+            >
+            <span className="material-symbols-outlined text-xl">delete</span>
+            Remove
+            </button>
         </div>
-      </div>
-
-      <div className="flex justify-end">
-        <button 
-          onClick={() => toggleFavorite(city.id)}
-          className="flex items-center gap-2 text-sm font-medium text-danger hover:text-danger-strong dark:hover:text-danger"
-        >
-          <span className="material-symbols-outlined text-xl">delete</span>
-          Remove
-        </button>
-      </div>
-    </div>
+        </div>
+    </Link>
   );
 };
